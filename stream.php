8c0e3c5385dc7977426e583c56882b555e5af106
gopibabu srungavarapu <gopi.srungavarapu@rotary.org>
2020-06-06 16:08:08 -0500

    added streams example

--- stream.php
+++ stream.php
@@ -7,11 +7,12 @@ use \React\Stream\ReadableResourceStream;
 
 $loop = Factory::create();
 
+// We can built interactive console applications using Streams
 $out = new WritableResourceStream(STDOUT, $loop);
 $in = new ReadableResourceStream(STDIN, $loop);
 
 $in->on('data', function ($data) use ($out) {
-    $out->write('This is output' . $data);
+    $out->write('This is output: ' . strtoupper($data));
 });
 
 $loop->run();
